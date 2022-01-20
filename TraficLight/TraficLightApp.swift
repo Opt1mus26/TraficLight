909d8481bf1226f62779032bd6587aca73fa20e8
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2022-01-20 21:31:39 +0300

    Initial Commit

---
+++ TraficLight/TraficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightApp.swift
+//  TraficLight
+//
+//  Created by Pavel Tsyganov on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
